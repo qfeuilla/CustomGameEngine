ff0fbeb13072878c4c2022cee7d9aba0c732e9a4
Quentin FEUILLADE--MONTIXI <quentin.feuillade33@gmail.com>
2020-07-25 21:39:13 +0200

    adding nice base object

--- renderer-test/PhongPS.hlsl
+++ renderer-test/PhongPS.hlsl
@@ -22,7 +22,7 @@ float4 main(float3 relativePos : Position, float3 n : Normal) : SV_Target
 	const float3 vToL = lightPos - relativePos;
 	const float distToL = length(vToL);
 	const float3 dirToL = vToL / distToL;
-	// diffuse attenuation
+	// attenuation
 	const float att = 1.0f / (attConst + attLin * distToL + attQuad * (distToL * distToL));
 	// diffuse intensity
 	const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f, dot(dirToL, n));
@@ -30,7 +30,7 @@ float4 main(float3 relativePos : Position, float3 n : Normal) : SV_Target
 	const float3 w = n * dot(vToL, n);
 	const float3 r = w * 2.0f - vToL;
 	// calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
-	const float3 specular = (diffuseColor * diffuseIntensity) * specularIntensity * pow(max(0.0f, dot(normalize(-r), normalize(relativePos))), specularPower);
+	const float3 specular = att * (diffuseColor * diffuseIntensity) * specularIntensity * pow(max(0.0f, dot(normalize(-r), normalize(relativePos))), specularPower);
 	// final color
 	return float4(saturate((diffuse + ambient + specular) * materialColor), 1.0f);
 }
